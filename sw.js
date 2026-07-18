383a52e38b22e0f0b7eb90f7d111bdd2e227abc6
lperkins72 <lperkins72@gmail.com>
2026-07-18 01:37:43 -0500

    Restore region-specific service worker paths

--- sw.js
+++ sw.js
@@ -35,14 +35,14 @@ const APP_SHELL = [
   "data/footer.json",
   "data/primaryending.json",
   "data/sync.json",
-  "regions/reg01/data/primary-assets.json",
-  "regions/reg01/data/primary.json",
-  "regions/reg01/data/footer.json",
-  "regions/reg01/data/primaryending.json",
-  "regions/reg01/assets/primary/manifest.json",
-  "regions/reg01/assets/footer/manifest.json",
-  "devices/reg01/index.html",
-  "devices/reg01/nuc-001.html"
+  "regions/reg10/data/primary-assets.json",
+  "regions/reg10/data/primary.json",
+  "regions/reg10/data/footer.json",
+  "regions/reg10/data/primaryending.json",
+  "regions/reg10/assets/primary/manifest.json",
+  "regions/reg10/assets/footer/manifest.json",
+  "devices/reg10/index.html",
+  "devices/reg10/nuc-001.html"
 ];
 
 const MEDIA_EXTENSIONS = new Set([
